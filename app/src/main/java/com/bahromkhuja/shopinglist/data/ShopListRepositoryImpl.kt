039b8e0b037967cf449f0e7933f16b4522694b97
Bahromkhuja <bahromhuca@gmail.com>
2023-01-26 17:40:42 +0500

    data layout

---
+++ app/src/main/java/com/bahromkhuja/shopinglist/data/ShopListRepositoryImpl.kt
@@ -0,0 +1,36 @@
+package com.bahromkhuja.shopinglist.data
+
+import com.bahromkhuja.shopinglist.domain.ShopItem
+import com.bahromkhuja.shopinglist.domain.ShopListRepository
+import java.lang.RuntimeException
+
+object ShopListRepositoryImpl: ShopListRepository {
+    private val shopList = mutableListOf<ShopItem>()
+    private var autoIncrementId = 0
+
+    override fun getShopList(): List<ShopItem> {
+        return shopList.toList()
+    }
+
+    override fun deleteShopItem(shopItem: ShopItem) {
+        shopList.remove(shopItem)
+    }
+
+    override fun getShopItem(shopItemId: Int): ShopItem {
+        return shopList.find { it.id == shopItemId } ?: throw RuntimeException("Element with id $shopItemId not found")
+    }
+
+    override fun addShopItem(shopItem: ShopItem) {
+        if (shopItem.id == ShopItem.UNDEFINED_ID) {
+            shopItem.id = autoIncrementId
+            autoIncrementId++
+        }
+        shopList.add(shopItem)
+    }
+
+    override fun editShopItem(shopItem: ShopItem) {
+        val oldElement = getShopItem(shopItem.id)
+        shopList.remove(oldElement)
+        addShopItem(shopItem)
+    }
+}
